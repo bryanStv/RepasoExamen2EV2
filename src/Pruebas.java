460b7710e6fc0ae11417c9b2457405ec201a3b5a
bryanStv <bryans.oyola@gmail.com>
2024-02-26 02:21:18 +0100

    Pruebas 1.0

---
+++ src/Pruebas.java
@@ -0,0 +1,7 @@
+public class Pruebas {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100;i++){
+            System.out.println((int)(Math.random()*6)+1);
+        }
+    }
+}
